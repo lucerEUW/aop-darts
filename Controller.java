a37290dc7bd61f9617a4b1c421d083dafe0e8e70
lucerEUW <149318953+lucerEUW@users.noreply.github.com>
2024-08-28 15:26:01 +0200

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,6 +1,7 @@
 package vlKlapptDart;
 
 import java.awt.Color;
+import java.awt.Graphics;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -30,47 +31,30 @@ public class Controller {
 	
   
     private void gameLoop() {
-    	Scanner reader = new Scanner(System.in);
-		
-		System.out.println("Coordinates go from 0.00 to 0.99, 0.50 is the center");
-		System.out.println("Write Coordinates like this: \"1356\" : x = 0.13 y = 0.56");
+    	Throw currentThrow = new Throw();
+
 		while(!score.isGameOver()) { 
+			
 			int player = score.getCurrentPlayerIndex(); // player count = 2
 			// example how to highlite a player:
 			SwingUtilities.invokeLater(() -> View.playerDisplayPanel.highlightPlayer(player));
-			for(int i = 0; i< 3; i++) {
-				String input = reader.nextLine();
+			
+			for(int i = 0; i < 3; i++) {
+		        
+				currentThrow.startThrow(view, score);
 				
-				
-				if(input.equals("c")) {
-					// example how to clear all darts:
-					SwingUtilities.invokeLater(() -> View.dartboard.clearDarts());
-					// example how to clear highlits:
-					SwingUtilities.invokeLater(() -> View.playerDisplayPanel.resetHighlighting());
-				}else {
-					input = input + "0000";
-					int x = Integer.parseInt(input.substring(0, 2));
-					int y = Integer.parseInt(input.substring(2, 4));
-					int points = calculator.checkHit(x, y);
+				SwingUtilities.invokeLater(() -> View.playerDisplayPanel.resetHighlighting());
 					
-					// EXAMPLE
-					float xFloat = ((float)x)/100;
-					float yFloat = ((float)y)/100;
-					// example for the aiming indicators
-					SwingUtilities.invokeLater(() -> View.dartboard.setAimingPosition(xFloat, yFloat));
-					//FOR LUCI
+				int x = (int) currentThrow.xValue;
+				int y = (int) currentThrow.yValue;
+				System.out.println(x + ":" + y);
+				int points = calculator.checkHit(x, y);
 					
-					// example how to throw a dart:
-					SwingUtilities.invokeLater(() -> {
-                        // Update the player's score
-                        score.updateScore(points);
-                        System.out.println("Points: "+ points+ " Player: " + score.getCurrentPlayerIndex() + " Konto neu: " + score.getScore());
-                        // Update GUI elements synchronously
-                        View.dartboard.addDart((float) x / 100, (float) y / 100, score.getCurrentPlayer().getColor());
-                        View.playerDisplayPanel.setPointsOf(score.getCurrentPlayerIndex(), score.getScore());
-                    });
-				}	
-			}
+                score.updateScore(points);
+                System.out.println("Points: "+ points+ " Player: " + score.getCurrentPlayerIndex() + " Konto neu: " + score.getScore());
+                // Update GUI elements synchronously
+                View.playerDisplayPanel.setPointsOf(score.getCurrentPlayerIndex(), score.getScore());
+		}
 			
 			synchronized (this) {
 	            SwingUtilities.invokeLater(() -> {
